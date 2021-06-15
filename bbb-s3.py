d43562a089e71f462944d81a6d1b286f90778d7c
Aldee-mu-zero <76679704+Aldee-mu-zero@users.noreply.github.com>
2021-06-15 19:03:46 +0530

    Update bbb-s3.py

--- bbb-s3.py
+++ bbb-s3.py
@@ -4,44 +4,16 @@ mime = magic.Magic(mime=True)
 ## Configuration Part 
 BUCKET_NAME = 'bbbrecording'
 DELETE_SERVER_FILES = True ## Set False (F should be capital) if you don't want to delete files from bbb-server
+dev = boto3.session.Session(profile_name='awsrecording')
+s3 = dev.resource('s3')
 
 
-def create_presigned_post(bucket_name, object_name,
-                          fields=None, conditions=None, expiration=3600):
-    """Generate a presigned URL S3 POST request to upload a file
-
-    :param bucket_name: string
-    :param object_name: string
-    :param fields: Dictionary of prefilled form fields
-    :param conditions: List of conditions to include in the policy
-    :param expiration: Time in seconds for the presigned URL to remain valid
-    :return: Dictionary with the following keys:
-        url: URL to post to
-        fields: Dictionary of form fields and values to submit with the POST
-    :return: None if error.
-    """
-
-    # Generate a presigned S3 POST URL
-    dev = boto3.session.Session(profile_name='bbbrecords')
-    s3_client = dev.client('s3')
-    try:
-        response = s3_client.generate_presigned_post(bucket_name,
-                                                     object_name,
-                                                     Fields=fields,
-                                                     Conditions=conditions,
-                                                     ExpiresIn=expiration)
-    except ClientError as e:
-        logging.error(e)
-        return None
-
-    # The response contains the presigned URL and required fields
-    return response
 
 def upload_to_aws(local_file, bucket, s3_file):
-    ctype = mime.from_file(local_file) 
-    response = create_presigned_post(bucket, s3_file, ExtraArgs={'ContentType': ctype})
+    ctype = mime.from_file(local_file)
+    data = open(s3_file, 'rb')
     try:
-        http_response = requests.post(response['url'], data=response['fields'], files=files)
+        s3.Bucket('bbbrecording').put_object(Key=s3_file, Body=data, ContentType=ctype)
         return True
     except OSError as e:
         if e.errno == errno.ENOENT:
